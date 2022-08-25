93410a2978691432987f4bda97eadaa330fad9a1
mercs600 <a.marcinkowski@macopedia.com>
2022-08-25 16:59:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-vkg67s
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-vkg67s)
---
+++ error.vue
@@ -0,0 +1,6 @@
+<template>
+  <p>404</p>
+</template>
+<script>
+console.log('calling from error');
+</script>
